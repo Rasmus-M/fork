9a26fdf7618eb244a6d91fec372804588d9f57e6
Rasmus-M <rasmus.moustgaard@gmail.com>
2017-12-30 16:21:10 +0100

    Initial commit

---
+++ make.bat
@@ -0,0 +1,9 @@
+IF EXIST skyway.dsk GOTO :dskok
+xdm99.py 4kcompo.dsk --initialize DSSD -n 4KCOMPO
+:dskok
+
+xas99.py -R -L 4kcompo.lst source/4kcompo.a99
+
+xas99.py -R -i source/4kcompo.a99 -o 4kcompo
+
+xdm99.py 4kcompo.dsk -a 4kcompo -n 4KCOMPO
