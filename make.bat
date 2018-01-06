e82e6054ce20a2fe15d284d87ab142196acbf6b7
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-01-06 13:51:04 +0100

    Char set

--- make.bat
+++ make.bat
@@ -7,3 +7,5 @@ xas99.py -R -L 4kcompo.lst source/4kcompo.a99
 xas99.py -R -i source/4kcompo.a99 -o 4kcompo
 
 xdm99.py 4kcompo.dsk -a 4kcompo -n 4KCOMPO
+
+java -jar tools/ea5tocart.jar 4kcompo "4K COMPO" > make.log
