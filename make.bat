6ccc84417f9de4ad651958b706cc37eb4704001a
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-01-20 22:36:45 +0100

    Build stuff

--- make.bat
+++ make.bat
@@ -1,11 +1,26 @@
+rem disk image
 IF EXIST skyway.dsk GOTO :dskok
 xdm99.py 4kcompo.dsk --initialize DSSD -n 4KCOMPO
 :dskok
 
-xas99.py -R -S -L 4kcompo.lst source/4kcompo.a99
+rem object file
+xas99.py -R -S -L 4kcompo.lst source/disk.a99
 
-xas99.py -R -i source/4kcompo.a99 -o 4kcompo
-
+rem E/A#5 file
+xas99.py -R -i -o 4kcompo source/disk.a99
 xdm99.py 4kcompo.dsk -a 4kcompo -n 4KCOMPO
 
-java -jar tools/ea5tocart.jar 4kcompo "4K COMPO" > make.log
+rem clean up disk
+IF EXIST 4kcompo.bin_6000 (
+del 4kcompo.bin_6000
+)
+IF EXIST 4kcompo.bin (
+del 4kcompo.bin
+)
+
+rem cart
+xas99.py -b -R -D cart -o 4kcompo.bin source/cart.a99
+ren 4kcompo.bin_6000 4kcompo.bin
+
+rem 32k cart
+rem java -jar tools/ea5tocart.jar 4kcompo "4K COMPO" > make.log
