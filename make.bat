8cce1dd69165d239ceeff218c34cc440e97c81a5
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-01-29 20:58:06 +0100

    Variable number of enemies

--- make.bat
+++ make.bat
@@ -1,16 +1,16 @@
-rem disk image
-IF EXIST skyway.dsk GOTO :dskok
+@rem disk image
+IF EXIST 4kcompo.dsk GOTO :dskok
 xdm99.py 4kcompo.dsk --initialize DSSD -n 4KCOMPO
 :dskok
 
-rem object file
+@rem object file
 xas99.py -R -S -L 4kcompo.lst -o 4kcompo.obj source/disk.a99
 
-rem E/A#5 file
+@rem E/A#5 file
 xas99.py -R -i -o 4kcompo source/disk.a99
 xdm99.py 4kcompo.dsk -a 4kcompo -n 4KCOMPO
 
-rem clean up
+@rem clean up
 IF EXIST 4kcompo.bin_6000 (
 del 4kcompo.bin_6000
 )
@@ -18,9 +18,9 @@ IF EXIST 4kcompo.bin (
 del 4kcompo.bin
 )
 
-rem cart
+@rem cart
 xas99.py -b -R -D cart -o 4kcompo.bin source/cart.a99
 ren 4kcompo.bin_6000 4kcompo.bin
 
-rem 32k cart
+@rem 32k cart
 java -jar tools/ea5tocart.jar 4kcompo "4K COMPO" > make.log
