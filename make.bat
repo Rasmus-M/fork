42e655110bcaa86a9b23717d0b873906d69f122c
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-01-29 21:56:30 +0100

    make

--- make.bat
+++ make.bat
@@ -6,9 +6,13 @@ xdm99.py 4kcompo.dsk --initialize DSSD -n 4KCOMPO
 @rem object file
 xas99.py -R -S -L 4kcompo.lst -o 4kcompo.obj source/disk.a99
 
-@rem E/A#5 file
+@rem E/A#5 file at >A000
 xas99.py -R -i -o 4kcompo source/disk.a99
 xdm99.py 4kcompo.dsk -a 4kcompo -n 4KCOMPO
+
+@rem object file at >7118
+xas99.py -R -D MINIMEM -L 4kcompomm.lst -o 4kcompomm.obj source/minimem.a99
+xdm99.py 4kcompo.dsk -a 4kcompomm.obj -n 4KCOMPOMM
 
 @rem clean up
 IF EXIST 4kcompo.bin_6000 (
